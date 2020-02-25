6eb5dae75df823883f938fcf7de513796609ae7c
Viktar.Zanimonets <vzanimonets@gmail.com>
2020-02-25 14:02:41 +0300

    feat:transform-array fn implementation

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,20 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error();
+
+  let resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      i++;
+    } else if (arr[i] === "--discard-prev") {
+      resultArr.pop();
+    } else if (arr[i] === "--double-next") {
+      resultArr.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev") {
+      resultArr.push(arr[i - 1]);
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+
+  return resultArr.filter(e => e !== undefined);
 };

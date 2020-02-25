0bef172757ea484a23735d67c5b6965c7551cf5b
Viktar.Zanimonets <vzanimonets@gmail.com>
2020-02-25 13:41:59 +0300

    feat: recursive-depth fn implementation

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,8 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) return 0;
+    return (
+      1 + arr.reduce((acc, cur) => Math.max(this.calculateDepth(cur), acc), 0)
+    );
+  }
 };

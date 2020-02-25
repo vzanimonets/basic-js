b651bec8e4dbd49ad0789011ad07ab319fd250eb
Viktar.Zanimonets <vzanimonets@gmail.com>
2020-02-25 12:46:45 +0300

    feat: repeater fn implementatiom

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "+"
+  } = options;
+
+  let result = "";
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += addition;
+      j < additionRepeatTimes - 1 ? (result += additionSeparator) : 0;
+    }
+    i < repeatTimes - 1 ? (result += separator) : 0;
+  }
+
+  return result;
 };
-  
